f0ee7025e7d754044c8273367e92a597e0035d9c
Chernov Ruslan <chernruslan@hotmail.com>
2017-08-22 17:05:32 +0700

    Create server, start web page

---
+++ app.js
@@ -0,0 +1,23 @@
+var express = require('express'),
+    app = express(),
+    path = require('path'),
+    config = {
+        port: 3000,
+        host: 'localhost',
+    };
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
+
+app.get('/', function(req, res) {
+  res.send('Hello World!');
+});
+
+app.get('/home', function(req, res){
+  res.sendFile(path.join(__dirname, 'views', 'home.html'));
+  //__dirname : It will resolve to your project folder.
+});
+
+app.listen(3000, function () {
+  console.log('Server starting on ' + config.host + ':' + config.port);
+});
